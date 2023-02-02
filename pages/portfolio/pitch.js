f4f4476cdf226d9fcc5d12c2b70bb98b9993e9b6
mattydy <68432907+mattydy@users.noreply.github.com>
2023-02-02 16:38:21 +0000

    pitch updates

--- pages/portfolio/pitch.js
+++ pages/portfolio/pitch.js
@@ -42,6 +42,20 @@ export default function Home(props) {
       <PortfolioIntro data={posts}></PortfolioIntro>
 
       <div className={styles.portfolioContent}>
+      
+          <div className={styles.highlights}>
+          <h3>Highlighted Features</h3>
+          <p>Below I have listed some key features on the website. They include parts of the website which the client whiched to change from the original design, elements that I have coded to save time and repeation and areas of the site that were key for functionality/user experience.</p>
+          </div>
+      
+        <div className={styles.feature}>
+          <div className={styles.image}></div>
+          
+          <div className={styles.content}>
+          <h4>Instant Impact</h4>
+          <p>One of the key features of the Pitch site is the Imagry. When you first connect to teh site you are presented with a slider of high definition images, giving the users a clear indication of the type of clientele Pitch work with.</p>
+          </div>
+        </div>
 
       </div>
 
