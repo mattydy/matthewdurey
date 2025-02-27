74a11a55797cb4d9e047b4ef5c937bb3c9a301e1
Matthew Durey <mattydy@hotmail.com>
2025-02-27 22:47:04 +1100

    changing api

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,6 +1,6 @@
 const mail = require('@sendgrid/mail');
 
-mail.setApiKey(process.env.SENDGRID_API_KEY);
+mail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
 export default (req, res) => {
     const body = JSON.parse(req.body);
